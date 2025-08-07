21810015ef5fd0c27bfbc2980ca8291b5aa6684c
Luis Gomez <lgomez00714@gmail.com>
2025-08-07 14:57:10 -0700

    Add education, skills, and summary sections to resume editor. Introduced new forms for managing education and skills, and updated validation schemas accordingly. Enhanced step navigation to include new sections for a comprehensive resume experience.

---
+++ src/app/(main)/editor/forms/SkillsForm.tsx
@@ -0,0 +1,67 @@
+import { Form,FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { EditorFormProps } from "@/lib/types";
+import { skillsSchema, SkillsValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import {  useForm } from "react-hook-form";
+
+export default function SkillsForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SkillsValues>({
+    resolver: zodResolver(skillsSchema),
+    defaultValues: {
+      skills: resumeData.skills || [],
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...resumeData,
+        skills:
+          values.skills
+            ?.filter((skill) => skill !== undefined)
+            .map((skill) => skill.trim())
+            .filter((skill) => skill !== "") || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+
+  return <div className="mx-auto max-w-xl space-y-6">
+    <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Skills</h2>
+        <p className="text-muted-foreground text-sm">Add your skills</p>
+    </div>
+    <Form {...form}>
+        <form className="space-y-3">
+            <FormField
+                control={form.control}
+                name="skills"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel className="sr-only">Skills</FormLabel>
+                        <FormControl>
+                            <Textarea {...field} placeholder="e.g. React, Node.js, etc..." 
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                const skills = value.split(",");
+                                field.onChange(skills);
+                            }}/> 
+                        </FormControl>
+                        <FormDescription>
+                            Enter your skills <span className="font-bold">separated by commas</span>
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+        </form>
+    </Form>
+  </div>;
+}
---
+++ src/app/(main)/editor/forms/SummaryForm.tsx
@@ -0,0 +1,62 @@
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { EditorFormProps } from "@/lib/types";
+import { summarySchema, SummaryValues } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import {  useForm } from "react-hook-form";
+
+export default function SummaryForm({
+  resumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<SummaryValues>({
+    resolver: zodResolver(summarySchema),
+    defaultValues: {
+      summary: resumeData.summary || "",
+    },
+  });
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({ ...resumeData, ...values });
+    });
+    return unsubscribe;
+  }, [form, resumeData, setResumeData]);
+  return (
+    <div className="mx-auto max-w-xl space-y-6">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-semibold">Professional Summary</h2>
+        <p className="text-muted-foreground text-sm">
+          Write a short introduction for your resume or let AI generate one from
+          your entered information
+        </p>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3">
+          <FormField
+            control={form.control}
+            name="summary"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="sr-only">Professional Summary</FormLabel>
+                <FormControl>
+                  <Textarea {...field} placeholder="e.g. I am a software engineer with 5 years of experience in web development..." />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </div>
+  );
+}
